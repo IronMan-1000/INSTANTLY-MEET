33cb45c2222ec8c7b795faad30ac39bf30f4a26d
Sagnik Biswas <72261318+IronMan-1000@users.noreply.github.com>
2021-10-17 13:29:56 +0530

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
 //All the constants used throughout the app declared here
 
 export const APP_NAME = 'Sampark';
-export const APP_URL = 'https://sampark-video-calls.netlify.app';
+export const APP_URL = 'https://instantly-meet.netlify.app/';
 export const BACKEND_URL = 'https://sampark-video.herokuapp.com/';
 // export const BACKEND_URL='http://localhost:5000';
